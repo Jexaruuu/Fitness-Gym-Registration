3c14e09b0f774702ee5623fcb9cc78a1d6112302
Jex Barsana <barsanajexter@gmail.com>
2024-10-30 00:18:06 +0800

    check

--- backend/controllers/fitnessController.js
+++ backend/controllers/fitnessController.js
@@ -1,34 +1,36 @@
+const db = require('../db');
 
-const db = require('../db'); 
-
-
-exports.getRegister = (req, res) => {
-  const sql = 'SELECT * FROM tbl_fitness'; 
-  db.query(sql, (err, result) => {
+const handleQuery = (res, sql, params, successMessage) => {
+  db.query(sql, params, (err, result) => {
     if (err) {
       return res.status(500).json({ error: err });
     }
-    res.json(result);
+    res.json(successMessage ? { message: successMessage, ...result } : result);
   });
 };
 
+exports.getRegister = (req, res) => {
+  const sql = 'SELECT * FROM tbl_fitness';
+  handleQuery(res, sql, [], null);
+};
 
 exports.addRegister = (req, res) => {
-  const { name, phone_number, email, btc, best_time } = req.body; 
+  const { name, phone_number, email, btc, best_time } = req.body;
 
-  if (!name || !phone_number || !email || !btc || !best_time) {
-    return res.status(400).json({ error: 'Please fill up the forms.' });
+  const missingFields = [];
+  if (!name) missingFields.push('name');
+  if (!phone_number) missingFields.push('phone_number');
+  if (!email) missingFields.push('email');
+  if (!btc) missingFields.push('btc');
+  if (!best_time) missingFields.push('best_time');
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({ error: `Please fill in the following fields: ${missingFields.join(', ')}` });
   }
-
 
   const sql = `
     INSERT INTO tbl_fitness (name, phone_number, email, btc, best_time) 
     VALUES (?, ?, ?, ?, ?)`;
-  
-  db.query(sql, [name, phone_number, email, btc, best_time], (err, result) => {
-    if (err) {
-      return res.status(500).json({ error: err });
-    }
-    res.json({ message: 'Registration Successful!', studentId: result.insertId });
-  });
+
+  handleQuery(res, sql, [name, phone_number, email, btc, best_time], 'Registration Successful!');
 };
